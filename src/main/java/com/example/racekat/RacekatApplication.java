d5ed84b660564641d2fa0f11fe1494151deb4a88
Al-khudri <mohammad.alkhudri@outlook.dk>
2025-03-31 10:16:32 +0200

    aa

---
+++ src/main/java/com/example/racekat/RacekatApplication.java
@@ -0,0 +1,13 @@
+package com.example.racekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacekatApplication.class, args);
+	}
+
+}
