7863957f2baef7ff35b5605c86dbc48b5a53416c
Al-khudri <mohammad.alkhudri@outlook.dk>
2025-03-31 10:49:25 +0200

    Update RacekatController.java

--- src/main/java/com/example/racekat/presentation/RacekatController.java
+++ src/main/java/com/example/racekat/presentation/RacekatController.java
@@ -2,12 +2,17 @@ package com.example.racekat.presentation;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RacekatController {
 
     @GetMapping("/login")
     public String showLogin(Model model){
-        model.addAttribute("user", new )
+        model.addAttribute("user", new User());
+        return "login";
     }
+
+    @PostMapping("/login")
+    public String login();
 }
