75d63842b146b0a72b1669e8540821318a30952e
BabaVamp <122454747+BabaVamp@users.noreply.github.com>
2025-04-01 17:40:13 +0200

    tirsdag

--- src/main/java/com/example/racekat/infrastucture/DatabaseConfig.java
+++ src/main/java/com/example/racekat/infrastucture/DatabaseConfig.java
@@ -1,13 +1,13 @@
 package com.example.racekat.infrastucture;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
     @Bean
+
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
