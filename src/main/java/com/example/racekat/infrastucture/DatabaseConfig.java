49ab145586bc28d2b5a7a875d19725922f6d5549
Al-khudri <mohammad.alkhudri@outlook.dk>
2025-04-08 20:13:59 +0200

    Update DatabaseConfig.java

--- src/main/java/com/example/racekat/infrastucture/DatabaseConfig.java
+++ src/main/java/com/example/racekat/infrastucture/DatabaseConfig.java
@@ -10,7 +10,7 @@ public class DatabaseConfig {
 
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.cj.mysql.jdbc.Driver");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/CatManagementSystem");
         dataSource.setUsername("root");
         dataSource.setPassword("Root");
